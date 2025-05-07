0afd90c92b6cb93a4dffe293c3335096e0db559d
PorschePot <Nantawat.thic@bumail.net>
2025-05-08 04:34:54 +0700

    --

--- Assets/Player/PlayerScript/PlayerCamera.cs
+++ Assets/Player/PlayerScript/PlayerCamera.cs
@@ -12,22 +12,16 @@ public class PlayerCamera : NetworkBehaviour
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
+    
+    
 
     public override void OnNetworkSpawn()
     {
         if (IsServer)
         {
-            var server = HostSingleton.Instance?.GameManager?.NetworkServer;
-            if (server != null)
-            {
-                var userData = server.GetUserDataByClientId(OwnerClientId);
-                Debug.Log($"[SetName] userName = {userData.userName}");
-                PlayerName.Value = userData.userName;
-            }
-            else
-            {
-                Debug.LogError("[PlayerCamera] NetworkServer not ready yet!");
-            }
+            UserData userData = HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
+
+            PlayerName.Value = userData.userName;
         }
 
         if (IsOwner)
