3a232040f9a7735b48e882786945aeaaabb8715a
Phoomeieiza <phoomiphat.kajh@bumail.net>
2025-05-08 02:16:46 +0700

    AnimAddCarabao

--- Assets/Player/PlayerScript/PlayerMovement2D.cs
+++ Assets/Player/PlayerScript/PlayerMovement2D.cs
@@ -10,26 +10,36 @@ public class PlayerMovement2D : NetworkBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private AudioSource jumpAudio;
+    
+    Animator animator;
 
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         Debug.Log($"{gameObject.name} | IsOwner: {IsOwner} | IsLocalPlayer: {IsLocalPlayer} | IsServer: {IsServer}");
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (!IsOwner) return;
 
+        animator.SetBool("IsJump", false);
         float move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
-
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        Debug.Log("Speed: " + Mathf.Abs(rb.velocity.x));
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            animator.SetBool("IsJump", true);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             if (jumpAudio != null)
                 jumpAudio.Play();
         }
+        
+        
+      
     }
 
     bool IsGrounded()
